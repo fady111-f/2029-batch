a74a3d14f8a8903b211edc1cd27e3f97f1cd5cb6
fady111-f <fady.fawzy2006@gmail.com>
2025-09-12 15:18:44 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Service Worker for offline support
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("cs2029-cache").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./logo.png",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
